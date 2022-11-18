e2bbe34e053d904b5978137dbfdf155faf301810
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2022-11-18 16:21:31 -0300

    Código refatorado: checagem de conexão com banco de dados antes de iniciar aplicação

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { resolve } = require('path');
 const { port } = require(resolve('src', 'config'));
 const consign = require('consign');
 const cors = require('cors');
+const { sequelize } = require(resolve('src', 'app', 'models'));
 
 app.use(cors());
 app.use(express.json());
@@ -12,4 +13,15 @@ consign()
   .include('src/controllers')
   .into(app);
 
-app.listen(port, ()=>console.log(`Application running at port ${port}`));
+new Promise(async (resolve, reject)=>{
+  try {
+      resolve(await sequelize.authenticate());
+      } catch (error) {
+      reject(error);
+  }
+})
+  .then(()=>{
+      console.log('A conexão com o banco de dados foi estabelecida com sucesso.');
+      app.listen(port, ()=>console.log(`Application running at port ${port}`));
+  })
+  .catch(error=>console.error('Não foi possível conectar ao banco de dados:', error));
