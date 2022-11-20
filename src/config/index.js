04d5aea7245baeafa96d4f699c815a80eb6814cc
Eduardo Marcelino <eduardo.marcelino.eng@gmail.com>
2022-11-19 21:37:53 -0300

    Dotenv foi configurado no projeto

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 //Variable that defines the current environment
-const env = "development";
+const env = "production";
+require('dotenv').config();
 
 //Environment settings development
 const development = {
@@ -17,9 +18,9 @@ const development = {
 const production = {
     port: 8080,
     database: {
-        "username": "root",
-        "password": "",
-        "database": "movie_catalog",
+        "username": process.env.USERNAMEDB,
+        "password": process.env.PASSWORDDB,
+        "database": process.env.DATABASEDB,
         "host": "127.0.0.1",
         "dialect": "mysql"
     }
